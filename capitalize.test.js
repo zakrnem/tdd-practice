7d325fadc75be1c3c9142e076d067941878f8ded
zakrnem <zakrnem@zohomail.com>
2023-06-26 20:57:02 -0600

    Reverse string f() passing tests

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,14 +1,14 @@
 import { capitalize } from "./capitalize"
 
-test("Capitalizes a single character string", () =>
+test("Capitalizes a single character string.", () =>
   expect(capitalize("a")).toEqual("A")
 )
 
-test("Capitalizes a multiple character string", () =>
+test("Capitalizes a multiple character string.", () =>
   expect(capitalize("ant")).toEqual("Ant")
 )
 
-test("Returns an error when entering a non string", () => {
+test("Throws error on non-string input.", () => {
     expect(() => capitalize(123)).toThrow(TypeError);
     expect(() => capitalize(['a', 'b','c'])).toThrow(TypeError);
   })
