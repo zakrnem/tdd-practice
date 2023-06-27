ecd0acd523f4fdf069c102e7c84e29785a7ca050
zakrnem <zakrnem@zohomail.com>
2023-06-26 20:21:42 -0600

    Capitalize function passing tests

--- capitalize.js
+++ capitalize.js
@@ -1,3 +1,12 @@
 export function capitalize(string) {
-  return string[0].toUpperCase()
+    if (typeof string !== 'string') {
+        throw new TypeError()
+    } else {
+        const firstLetter = string[0].toUpperCase()
+        let stringArray = [...string]
+        stringArray.splice(0,1)
+        const result = firstLetter + stringArray.join('')
+        return result
+    }
+    
 }
--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,12 @@ import { capitalize } from "./capitalize"
 test("Capitalizes a single character string", () =>
   expect(capitalize("a")).toEqual("A")
 )
+
+test("Capitalizes a multiple character string", () =>
+  expect(capitalize("ant")).toEqual("Ant")
+)
+
+test("Returns an error when entering a non string", () => {
+    expect(() => capitalize(123)).toThrow(TypeError);
+    expect(() => capitalize(['a', 'b','c'])).toThrow(TypeError);
+  })
