69726bd723be37e994d3dcc587d1825164e41830
zakrnem <zakrnem@zohomail.com>
2023-06-26 22:01:52 -0600

    Calculator f() passing tests

---
+++ calculator.js
@@ -0,0 +1,24 @@
+export function calculator(operation, firstNum, secondNum) {
+    if (typeof firstNum !== 'number' ||
+    typeof secondNum !== 'number') {
+        throw new TypeError()
+    } else {
+        switch(true) {
+            case (operation === "add"):
+                return firstNum+secondNum
+                break
+            case (operation === "subtract"):
+                return firstNum-secondNum
+                break
+            case (operation === "divide"):
+                return firstNum/secondNum
+                break
+            case (operation === "multiply"):
+                return firstNum*secondNum
+                break
+            default:
+                throw new Error('Invalid operation')
+        }
+    }
+    return
+}
---
+++ calculator.test.js
@@ -0,0 +1,31 @@
+import { calculator } from "./calculator"
+
+test("Adds two numbers.", () =>
+  expect(calculator('add', 2, 2)).toEqual(4),
+  expect(calculator('add', -2, 2)).toEqual(0)
+)
+
+test("Subtracts two numbers.", () =>
+  expect(calculator('subtract', 2, 2)).toEqual(0),
+  expect(calculator('subtract', -2, 2)).toEqual(-4)
+)
+
+test("Divides two numbers.", () =>
+  expect(calculator('divide', 2, 2)).toEqual(1),
+  expect(calculator('divide', -2, 2)).toEqual(-1)
+)
+
+test("Multiplies two numbers.", () =>
+  expect(calculator('multiply', 2, 2)).toEqual(4),
+  expect(calculator('multiply', -2, 2)).toEqual(-4)
+)
+
+test("Throws error on non-string input.", () => {
+    expect(() => calculator('add', '2', 2)).toThrow(TypeError);
+    expect(() => calculator('add', 2, '2')).toThrow(TypeError);
+    expect(() => calculator('add', [3], 2)).toThrow(TypeError);
+  })
+
+test("Throws error on invalid.", () => {
+    expect(() => calculator('calc', '2', 2)).toThrow(Error);
+})
