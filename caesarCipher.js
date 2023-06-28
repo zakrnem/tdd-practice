2f245df8e37e42df2d3ffb5a797d73301c96ee2c
zakrnem <zakrnem@zohomail.com>
2023-06-27 19:28:34 -0600

    caesarCipher() in progress

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,26 @@
-export function caesarCipher(string, offset) {
-  return
+export function caesarCipher(offset, string) {
+  const stringArray = Array.from(string)
+  stringArray.forEach((character, index, array) => {
+    array[index] = offsetChar(character, offset)
+  })
+  return stringArray.join('')
+}
+
+function offsetChar(character, offset) {
+  if (offset > 25) {
+    throw new Error()
+  }
+  if (character === ' ') return character
+  else {
+    character = character.toLowerCase()
+    let charPosition = character.charCodeAt()
+    if (charPosition >= 97 && charPosition <= 122) {//Lowercase
+      charPosition += offset
+      charPosition = (charPosition > 122) ? charPosition-26 : charPosition
+      charPosition = (charPosition < 97) ? charPosition+26 : charPosition
+      character = String.fromCharCode(charPosition)
+      return character
+    }
+    
+  }
 }
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -2,16 +2,23 @@ import { caesarCipher } from './caesarCipher'
 
 test(
     'Preservers the string if offset is equal to zero.',
-    () => expect(caesarCipher(0, 'Julius Caesar')).toEqual('Julius Caesar'),
+    () => expect(caesarCipher(0, 'julius caesar')).toEqual('julius caesar'),
 )
 
-test(
-    'Works.',
+ test(
+    'Ciphers the string sucessfully.',
     () => expect(caesarCipher(1, 'attack at dawn')).toEqual('buubdl bu ebxo'),
+    expect(caesarCipher(10, 'attack at dawn')).toEqual('kddkmu kd nkgx'),
     expect(caesarCipher(25, 'attack at dawn')).toEqual('zsszbj zs czvm')
 )
+
+test('Throws error on invalid offset.', () => {
+    expect(() => caesarCipher(26, 'Julius Caesar')).toThrow(Error)
+})  
+
+/*
 
 test('Throws error on non-string input.', () => {
     expect(() => caesarCipher('add', 'Julius Caesar')).toThrow(TypeError),
     expect(() => caesarCipher('Julius Caesar')).toThrow(TypeError)
-})
+}) */
